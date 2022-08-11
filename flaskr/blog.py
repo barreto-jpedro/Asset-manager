98acd088c3820e12a735a3db23e4ae0336758022
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-08-11 11:53:45 -0300

    Starting changes

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, amount, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -23,21 +23,23 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        ticker = request.form['ticker']
+        price = request.form['price']
+        amount = request.form['amount']
+        created = request.form['created']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not ticker:
+            error = 'Ticker is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (title, body, amount, created, author_id)'
+                ' VALUES (?, ?, ?, ?, ?)',
+                (ticker, price, amount, created, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -46,7 +48,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, amount, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -66,21 +68,24 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        ticker = request.form['ticker']
+        price = request.form['price']
+        amount = request.form['amount']
+        created = request.form['created']
+
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not ticker:
+            error = 'Ticker is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET title = ?, body = ?, amount = ?, created = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (ticker, price, amount,created, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
--- localrun.sh
+++ localrun.sh
@@ -1,1 +1,3 @@
+#By enabling debug mode, the server will automatically reload if code changes, 
+#and will show an interactive debugger in the browser if an error occurs during a request.
 flask --app flaskr --debug run
